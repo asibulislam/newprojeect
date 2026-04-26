21c2fb6e2b0d1af3c12de4d49e1c42f08d495446
Asibul Islam <asibulislam330@gmail.com>
2026-04-26 16:02:34 +0600

    added multiplication, subtraction, divition feature

--- mycal.py
+++ mycal.py
@@ -1,7 +1,20 @@
 a = int(input("Enter an int: "))
 b = int(input("Enter another int: "))
 
-print("Sum: ", a+b)
+op = input("Enter operation (+, -, *, /): ")
+
+if(op == '+'):
+    print("Sum: ", a+b)
+elif(op == '-'):
+    print("Subtraction: ", a-b)
+elif(op == '*'):
+    print("Multiplication: ",a*b)
+
+elif(op == '/'):
+    print("Division: ",a/2)
+
+
+
 
 
 
